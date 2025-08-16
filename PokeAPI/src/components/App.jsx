4378dced72ada92bd56773f843d459ce83e21ec7
Allan Eduardo Perez Ajanel <allanperezajanel@gmail.com>
2025-08-15 23:27:21 -0600

    start programming the app.jsx for proyect

--- PokeAPI/src/components/App.jsx
+++ PokeAPI/src/components/App.jsx
@@ -1,2 +1,11 @@
+import { useState } from "react";
+import "./App.css";
 
-
+function App() {
+  const [pokemons, setPokemons] = useState([]);
+  const [offset, setOffset] = useState(0); // Para paginación
+  const limit = 15; // Cantidad por página
+  const [loading, setLoading] = useState(false);
+  const [search, setSearch] = useState("");
+  const [page, setPage] = useState(1); // Página actual
+  const [notFound, setNotFound] = useState(false); // Pokémon no encontrado
