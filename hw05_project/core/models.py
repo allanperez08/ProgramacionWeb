a293c22b2a153fbd3611a478f5bb5b3029f0ff41
Allan Eduardo Perez Ajanel <allanperezajanel@gmail.com>
2025-09-10 16:30:12 -0600

    i finished the model Author

--- hw05_project/core/models.py
+++ hw05_project/core/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 class Author(models.Model):
+    name = models.CharField(max_length=100)
+    birthdate = models.DateField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
