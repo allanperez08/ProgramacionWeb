c7d52713741dc757f2d8ee72bde5ebafdc358c03
Allan Eduardo Perez Ajanel <allanperezajanel@gmail.com>
2025-10-07 22:29:14 -0600

    feat(setting.py) i edited the file settings for things about django)

--- hw05_project/hw05_project/settings.py
+++ hw05_project/hw05_project/settings.py
@@ -1,3 +1,26 @@
+import os
+from pathlib import Path
+from dotenv import load_dotenv
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+load_dotenv(os.path.join(BASE_DIR, '.env'))
+
+SECRET_KEY = os.getenv('SECRET_KEY', 'fallback-key')
+DEBUG = os.getenv('DEBUG', 'False') == 'True'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST', 'db'),
+        'PORT': os.getenv('DB_PORT', '5432'),
+    }
+}
+
+
+
 """
 Django settings for hw05_project project.
 
