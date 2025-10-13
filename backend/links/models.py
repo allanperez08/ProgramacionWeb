842a6aac163f818637ee5cd9c5e75a39fdfbb2c0
Allan Eduardo Perez Ajanel <allanperezajanel@gmail.com>
2025-10-12 18:46:49 -0600

    feat(backend) add the model SecretLink

--- backend/links/models.py
+++ backend/links/models.py
@@ -1,2 +1,12 @@
 from django.db import models
 import uuid
+
+class SecretLink(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    original_url = models.URLField()
+    secret_code = models.CharField(max_length=20, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    expires_at = models.DateTimeField()
+
+    def __str__(self):
+        return self.secret_code
