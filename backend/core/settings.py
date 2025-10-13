10f09aedf25bd584e48fe1041bf8adcf1ecfeb71
Allan Eduardo Perez Ajanel <allanperezajanel@gmail.com>
2025-10-12 18:53:44 -0600

    feat(backend) add core in urls

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'links',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
--- backend/core/urls.py
+++ backend/core/urls.py
@@ -16,7 +16,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.contrib import admin
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from links.views import SecretLinkViewSet
+
+router = DefaultRouter()
+router.register(r'links', SecretLinkViewSet)
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include(router.urls)),
 ]
